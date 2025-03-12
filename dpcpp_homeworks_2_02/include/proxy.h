50d7d290c150ba0e266f8a209bfaf02bc22df26e
Puzzanis <capitalsvk@gmail.com>
2025-03-12 07:25:15 +0300

    first commit

---
+++ dpcpp_homeworks_2_02/include/proxy.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include <map>
+#include <string>
+#include <iostream>
+
+class VeryHeavyDatabase {
+public:
+    std::string GetData(const std::string& key) const noexcept {
+        return "Very Big Data String: " + key;
+    }
+};
+
+class CacheProxyDB : VeryHeavyDatabase {
+public:
+    explicit CacheProxyDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        if (cache_.find(key) == cache_.end()) {
+            std::cout << "Get from real object\n";
+            cache_[key] = real_db_->GetData(key);
+        }
+        else {
+            std::cout << "Get from cache\n";
+        }
+        return cache_.at(key);
+    }
+
+private:
+    std::map<std::string, std::string> cache_;
+    VeryHeavyDatabase* real_db_;
+};
+
+class TestDB : VeryHeavyDatabase {
+public:
+    explicit TestDB(VeryHeavyDatabase* real_object) : real_db_(real_object) {}
+    std::string GetData(const std::string& key) noexcept {
+        return "test_data\n";
+    }
+private:
+    VeryHeavyDatabase* real_db_;
+};
+
+class OneShotDB
+{
+public:
+    explicit class OneShotDB(VeryHeavyDatabase* real_object, const int& limit) : real_db_(real_object), limit_(limit) {}
+    std::string GetData(const std::string& key) noexcept 
+    {
+        if (count_ < limit_)
+        {
+            count_ += 1;
+            return real_db_->GetData(key);
+        }
+        return "error\n";
+    }
+
+private:
+    int limit_;
+    int count_{ 0 };
+    VeryHeavyDatabase* real_db_;
+};
---
+++ dpcpp_homeworks_2_02/main.cpp
@@ -0,0 +1,22 @@
+﻿// dpcpp_homeworks_2_02.cpp: определяет точку входа для приложения.
+//
+
+#include "./include/proxy.h"
+
+using namespace std;
+
+int main()
+{
+    auto real_db = VeryHeavyDatabase();
+    auto cached_db = CacheProxyDB(std::addressof(real_db));
+    auto test_db = TestDB(std::addressof(real_db));
+    auto limit_db = OneShotDB(std::addressof(real_db), 2);
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << cached_db.GetData("key") << std::endl;
+    std::cout << test_db.GetData("key") << std::endl;
+
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    std::cout << limit_db.GetData("key") << std::endl;
+    return 0;
+}
