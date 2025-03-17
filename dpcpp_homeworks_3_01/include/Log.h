64ed47a72e5a57df612e0925aa1501ba8aa37ece
Puzzanis <capitalsvk@gmail.com>
2025-03-17 07:14:40 +0300

    first commit

---
+++ dpcpp_homeworks_3_01/include/Log.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include <iostream>
+
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class LogToConsol : public LogCommand
+{
+public:
+    void print(const std::string& message) override
+    {
+        std::cout << "print to Consol: " << message << std::endl;
+    }
+   
+};
+
+class LogToFile :public LogCommand
+{
+public:
+    void print(const std::string& message) override
+    {
+        std::cout << "print to File: " << message << std::endl;
+    }
+};
+
+class PrintAnywhere
+{
+private:
+    const std::string& message_;
+
+public:
+    PrintAnywhere(const std::string& message) : message_{ message } {}
+    void print(LogCommand& log)
+    {
+        log.print(message_);
+    }
+};
---
+++ dpcpp_homeworks_3_01/main.cpp
@@ -0,0 +1,17 @@
+﻿// dpcpp_homeworks_3_01.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include "./include/Log.h"
+
+int main()
+{
+	LogToConsol consol;
+	LogToFile files;
+
+	PrintAnywhere print("Hello world!");
+	print.print(consol);
+	print.print(files);
+
+
+	return 0;
+}
